85aae2f594e14a0bc375d7044369cb31be0d5541
Tristan Burke <tristan_burke@berkeley.edu>
2017-08-31 12:13:27 -0700

    first draft of discovery page - no functionality, just visual

--- src/app/discovery.component.ts
+++ src/app/discovery.component.ts
@@ -1,4 +1,12 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { HostListener } from '@angular/core';
+
+export enum KEY_CODE {
+  SPACE = 32,
+  RIGHT_ARROW = 39,
+  LEFT_ARROW = 37
+
+}
 
 @Component({
   selector: 'app-discovery',
@@ -7,5 +15,35 @@ import {Component} from '@angular/core';
 })
 
 export class DiscoveryComponent {
+  playing: boolean;
+  show_play_status: boolean;
 
+  constructor() {
+    this.playing = true;
+    this.show_play_status = false;
+
+  }
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    console.log(event);
+    if (event.keyCode === KEY_CODE.SPACE) {
+      this.toggle_playing()
+    }
+    else if (event.keyCode == KEY_CODE.RIGHT_ARROW) {
+
+    }
+    else if (event.keyCode == KEY_CODE.LEFT_ARROW) {
+
+    }
+  }
+  toggle_playing() : void {
+    this.playing = !this.playing;
+    this.show_playing_status();
+  }
+  show_playing_status() : void {
+    this.show_play_status = true;
+    setTimeout(() => {
+      this.show_play_status = false;
+    }, 1000);
+  }
 }
