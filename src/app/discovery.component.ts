037dfed2e147480014bdaf964b8739313e0069e8
Tristan Burke <tristan_burke@berkeley.edu>
2017-09-04 17:30:55 -0700

    created basic progress bar

--- src/app/discovery.component.ts
+++ src/app/discovery.component.ts
@@ -17,11 +17,13 @@ export enum KEY_CODE {
 export class DiscoveryComponent {
   playing: boolean;
   show_play_status: boolean;
+  progress_index: number;
 
   constructor() {
     this.playing = true;
     this.show_play_status = false;
-
+    this.progress_index = 0;
+    this.play()
   }
   @HostListener('window:keyup', ['$event'])
   keyEvent(event: KeyboardEvent) {
@@ -37,6 +39,9 @@ export class DiscoveryComponent {
     }
   }
   toggle_playing() : void {
+    if (!this.playing) {
+      this.play()
+    }
     this.playing = !this.playing;
     this.show_playing_status();
   }
@@ -45,5 +50,12 @@ export class DiscoveryComponent {
     setTimeout(() => {
       this.show_play_status = false;
     }, 1000);
+  }
+  play() : void {
+    for (let _i = 0; _i <= 100; _i++) {
+      setTimeout(() => {
+        this.progress_index += 1;
+      }, 500);
+    }
   }
 }
